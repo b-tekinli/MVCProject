9d16fa967c6593c583a16d4e0d00b5a42f1da6d6
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-05-17 04:31:39 +0300

    updated.

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adını boş bırakamazsınız!");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar soyadını boş bırakamazsınız!");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkımda kısmını boş bırakamazsınız!").Must(x => x.Contains('a')).WithMessage("Hakkımda kısmında 'a' karakteri kullanmalısınız!");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter giriniz!");
+            RuleFor(x => x.WriterSurname).MaximumLength(50).WithMessage("En fazla 50 karakter girebilirsiniz!");
+        }
+    }
+}
--- MVCDb/WritersData.sql
+++ MVCDb/WritersData.sql
@@ -1,7 +1,7 @@
 ﻿SET IDENTITY_INSERT [dbo].[Writers] ON
-INSERT INTO [dbo].[Writers] ([WriterID], [WriterName], [WriterSurname], [WriterImage], [WriterMail], [WriterPassword]) VALUES (1, N'Ali', N'Yıldız', NULL, N'aliyildiz@gmail.com', N'1')
-INSERT INTO [dbo].[Writers] ([WriterID], [WriterName], [WriterSurname], [WriterImage], [WriterMail], [WriterPassword]) VALUES (2, N'Mehmet', N'Çınar', NULL, N'mehmet@gmail.com', N'11')
-INSERT INTO [dbo].[Writers] ([WriterID], [WriterName], [WriterSurname], [WriterImage], [WriterMail], [WriterPassword]) VALUES (3, N'Aleyna', N'Öztürk', NULL, N'aleyna@gmail.com', N'123')
-INSERT INTO [dbo].[Writers] ([WriterID], [WriterName], [WriterSurname], [WriterImage], [WriterMail], [WriterPassword]) VALUES (4, N'Gizem', N'Ata', NULL, N'gizem@gmail.com', N'10')
-INSERT INTO [dbo].[Writers] ([WriterID], [WriterName], [WriterSurname], [WriterImage], [WriterMail], [WriterPassword]) VALUES (5, N'Aslı', N'Kaya', NULL, N'asli@gmail.com', NULL)
+INSERT INTO [dbo].[Writers] ([WriterID], [WriterName], [WriterSurname], [WriterImage], [WriterMail], [WriterPassword], [WriterAbout]) VALUES (1, N'Ali', N'Yıldız', NULL, N'aliyildiz@gmail.com', N'1', N'Yazılım Mühendisi, Frontend Developer')
+INSERT INTO [dbo].[Writers] ([WriterID], [WriterName], [WriterSurname], [WriterImage], [WriterMail], [WriterPassword], [WriterAbout]) VALUES (2, N'Mehmet', N'Çınar', NULL, N'mehmet@gmail.com', N'11', N'Senior Software Developer, bolca kitap okur.')
+INSERT INTO [dbo].[Writers] ([WriterID], [WriterName], [WriterSurname], [WriterImage], [WriterMail], [WriterPassword], [WriterAbout]) VALUES (3, N'Aleyna', N'Öztürk', NULL, N'aleyna@gmail.com', N'123', N'Tiyatro, filtre kahve')
+INSERT INTO [dbo].[Writers] ([WriterID], [WriterName], [WriterSurname], [WriterImage], [WriterMail], [WriterPassword], [WriterAbout]) VALUES (4, N'Gizem', N'Ata', NULL, N'gizem@gmail.com', N'10', N'Spor, bisiklet, dergiler ve karikatürler')
+INSERT INTO [dbo].[Writers] ([WriterID], [WriterName], [WriterSurname], [WriterImage], [WriterMail], [WriterPassword], [WriterAbout]) VALUES (5, N'Aslı', N'Kaya', NULL, N'asli@gmail.com', N'000', N'Kahve vazgeçilmezim, yanında bolca kod ile..')
 SET IDENTITY_INSERT [dbo].[Writers] OFF
